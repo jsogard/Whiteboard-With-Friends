3cc3f088de3b9dde9043aaf78132fcacf46c2e4a
Joe <jmsogard@gmail.com>
2016-12-02 14:06:49 -0600

    debug db

--- index.php
+++ index.php
@@ -6,24 +6,26 @@ echo "Starting page";
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = ;
+$server = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
-var_dump($url, $server, $username, $password, $db);
+//var_dump($url, $server, $username, $password, $db);
 
-/*
-$conn = new mysqli($server, $username, $password, $db);
 
-if($mysqli->connect_errno){
-	echo "Connection failed: ".$mysqli->connect_error;
+$conn = mysqli_connect($server, $username, $password, $db);
+
+if(!$conn){
+	echo "Connection failed: ".$mysqli_connect_error();
 }
 else
 {
 	echo "Connected!"
 }
-*/
+
+
+// mysql -h us-cdbr-iron-east-04.cleardb.net -u b9376310256763 -p935d09da heroku_d27a38e4352bbd4
 
 
 ?>
