8efec2310e7c51c3dcfd95b8aa83ed5e0c2a5cb6
Joe <jmsogard@gmail.com>
2016-12-02 12:58:17 -0600

    Testing db connection

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = ;
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+
+if($mysqli->connect_errno){
+	echo "Connection failed: ".$mysqli->connect_error;
+}
+
+?>
