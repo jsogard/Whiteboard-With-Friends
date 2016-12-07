5003659fe0bcc95c0a76ca9018dca83ae9047473
Joe <jmsogard@gmail.com>
2016-12-07 13:49:13 -0600

    Readapted for ISU webserver

--- index.php
+++ index.php
@@ -1,31 +1,15 @@
 <?php
 
-echo "Starting page";
+session_start();
 
-
-
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
-//var_dump($url, $server, $username, $password, $db);
-
-
-$conn = mysqli_connect($server, $username, $password, $db);
-
-if(!$conn){
-	echo "Connection failed: ".$mysqli_connect_error();
+if(isset($_SESSION['user']))
+{
+	echo $_SESSION['user']['username'];
 }
 else
 {
-	echo "Connected!"
+	session_destroy();
+	echo "";
 }
-
-
-// mysql -h us-cdbr-iron-east-04.cleardb.net -u b9376310256763 -p935d09da heroku_d27a38e4352bbd4
-
 
 ?>
